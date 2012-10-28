f5800160701d529db3eabb919a06e7061b484f79
Matthias Schiffer <mschiffer@universe-factory.net>
2012-10-28 11:43:55 +0100

    Break public schedule at separators

--- app/helpers/public/schedule_helper.rb
+++ app/helpers/public/schedule_helper.rb
@@ -1,7 +1,35 @@
 module Public::ScheduleHelper
 
+  def day_parts
+    separators = @day.day_separators.collect { |sep| sep.time }
+
+    starts = Array.new(separators)
+    starts.unshift @day.start_date
+
+    ends = Array.new(separators)
+    ends << @day.end_date
+
+    starts.zip(ends)
+  end
+
   def each_timeslot(&block)
     each_minutes(@conference.timeslot_duration, &block)
+  end
+
+  def each_part_timeslot((start_time, end_time), &block)
+    time = start_time
+    while time < end_time
+      yield time
+      time = time.since(@conference.timeslot_duration.minutes)
+    end
+  end
+
+  def used_rooms((start_time, end_time))
+    rooms = Array.new @conference.rooms.public
+
+    rooms.keep_if do |room|
+      @events[room].any? { |event| event.start_time >= start_time and event.start_time < end_time }
+    end
   end
 
   def track_class(event)
--- app/models/day_separator.rb
+++ app/models/day_separator.rb
@@ -5,6 +5,8 @@ class DaySeparator < ActiveRecord::Base
   validates_presence_of :time, :message => "missing time"
   validate :during_day, :message => "separator not during corresponding day"
 
+  default_scope order('time ASC')
+
   def during_day
     self.errors.add(:time, "should be after start date") if self.time <= self.day.start_date
     self.errors.add(:time, "should be before end date") if self.time >= self.day.end_date
